358dd00149e3a2b01dec4961544a1d3a9bedbebf
Berat <berqtguzel@gmail.com>
2025-01-12 19:04:18 +0300

    add responsive tailwindcss

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -112,8 +112,8 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-800 via-black to-gray-900 text-white flex">
-      <div className="w-72 bg-gray-700 p-6 rounded-lg m-4 flex flex-col justify-between shadow-lg">
+    <div className="min-h-screen bg-gradient-to-b from-gray-800 via-black to-gray-900 text-white flex flex-col md:flex-row">
+      <div className="w-full md:w-72 bg-gray-700 p-6 rounded-lg m-4 flex flex-col justify-between shadow-lg">
         <h2 className="text-lg font-bold text-orange-500 mb-4">
           Geçmiş Aramalar
         </h2>
@@ -138,16 +138,33 @@ export default function Home() {
             </div>
           ))}
         </div>
+        <div className="flex flex-row mt-2 md:flex-row md:space-y-0 md:space-x-4">
+          <button
+            onClick={handleLogout}
+            className="p-2 bg-gradient-to-r from-red-500 to-red-600 text-white rounded-full shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105 flex items-center space-x-2 text-sm"
+          >
+            <FiLogOut className="h-4 w-4" />
+            <span className="text-sm font-medium">Çıkış Yap</span>
+          </button>
+
+          <button
+            onClick={handleNewChat}
+            className="p-2 bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-full shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105 flex items-center space-x-2 text-sm"
+          >
+            <RiChat4Fill className="h-4 w-4" />
+            <span className="text-sm font-medium">Yeni Sohbet</span>
+          </button>
+        </div>
       </div>
 
-      <div className="flex-grow flex flex-col justify-between p-10">
+      <div className="flex-grow flex flex-col justify-between p-4 md:p-10">
         <div className="max-w-2xl mx-auto w-full">
-          <h1 className="text-5xl font-extrabold mb-6 text-center text-orange-400 drop-shadow-lg">
+          <h1 className="text-3xl md:text-5xl font-extrabold mb-6 text-center text-orange-400 drop-shadow-lg">
             AI-Writer
           </h1>
-          <div className="space-y-4 mb-6 overflow-y-auto max-h-[70vh] custom-scrollbar">
+          <div className="space-y-4 mb-6 overflow-y-auto max-h-[50vh] md:max-h-[70vh] custom-scrollbar">
             {currentChatHistory.map((item, index) => (
-              <div key={index} className="p-4 rounded-lg shadow-lg">
+              <div key={index} className="p-4 rounded-lg mb-4 shadow-lg">
                 <div className="bg-gray-800 p-4 rounded-lg mb-2">
                   <h2 className="text-lg font-bold text-orange-400">Siz:</h2>
                   <p className="mt-2">{item.input}</p>
@@ -163,7 +180,7 @@ export default function Home() {
           </div>
           <form
             onSubmit={handleSubmit}
-            className="flex space-x-4 w-120 bg-gradient-to-r from-gray-700 to-gray-800 p-5 rounded-xl shadow-lg fixed bottom-4 left-4 right-4"
+            className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full bg-gradient-to-r from-gray-700 to-gray-800 p-5 rounded-xl shadow-lg fixed bottom-4 left-4 right-4 md:static"
           >
             <input
               type="text"
@@ -191,24 +208,6 @@ export default function Home() {
             </div>
           )}
         </div>
-
-        <div className="fixed bottom-49 right-4 flex flex-col space-y-4">
-          <button
-            onClick={handleLogout}
-            className="p-4 bg-gradient-to-r from-red-500 to-red-600 text-white rounded-full shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105 flex items-center space-x-3"
-          >
-            <FiLogOut className="h-6 w-6" />
-            <span className="text-lg font-medium">Çıkış Yap</span>
-          </button>
-
-          <button
-            onClick={handleNewChat}
-            className="p-4 bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-full shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105 flex items-center space-x-3"
-          >
-            <RiChat4Fill className="h-6 w-6" />
-            <span className="text-lg font-medium">Yeni Sohbet</span>
-          </button>
-        </div>
       </div>
 
       <style jsx>{`
@@ -222,6 +221,23 @@ export default function Home() {
           background-color: #ff6600;
           border-radius: 6px;
           border: 3px solid #1a1a1a;
+        }
+
+        @media (max-width: 768px) {
+          .flex-grow {
+            padding: 10px;
+          }
+        }
+
+        @media (max-width: 480px) {
+          .fixed {
+            padding: 5px;
+            bottom: 16px;
+            left: 16px;
+            right: 16px;
+            flex-direction: row;
+            justify-content: space-between;
+          }
         }
       `}</style>
     </div>
